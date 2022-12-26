4970c37c4c6d7d62a3ae505f1dae5459412bd2b7
danieluzquiano <55982609+danieluzquiano@users.noreply.github.com>
2022-12-26 12:38:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
-t=1e4
-setup=_=>{createCanvas(w=windowWidth,h=windowHeight),noiseSeed(fxrand()*999999)}
-  draw=_=>{
-    for(
-      background(0),
-      translate(w/2,h/2),
-      scale(125),
-      noStroke(),
-      fill(255),
-      n=map(mouseX,0,w,0,3),
-      a=0;
-      a<5;
-      a+=4e-4
-    )
-    x=1e4*a,
-    y=cos(a)/sin(t/x),
-    e=cos(x)/a,
-    f=2*noise(tan(e)),
-    circle(sin(tan(cos(y))+n)*f*e,sin(tan(sin(y))+t)*f,.012);
-    t += .03
-  }; //#つぶやきProcessing
+let t=s=100000;
+let mX, mY
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  mX = windowWidth / 2;
+  mY = windowHeight / 2;
+}
+
+function draw() {
+  background(255);
+  translate(width / 2, height / 2);
+  scale(125);
+  let mm = map(mX, 0, width, 1, 5);
+  noStroke();
+  for (let a = 0; a < 5.5; a += 0.0004) {
+    fill(0);  
+    let x = a*10000;
+    let y = cos(a) / sin(t/x);
+    let e = cos(x)/a;
+    let f = 2*noise(tan(e));
+    circle(sin(tan(cos(y))+mm)*f*e, sin(tan(sin(y))+t)*f,0.012);
+  }
+  t += 0.035;
+  s +=0.003
+}
+
+function mousePressed() {
+  mX = mouseX;
+  mY = mouseY;
+}
+
+function mouseMoved() {
+  mX = mouseX;
+  mY = mouseY;
+}
+
+function touchStarted() {
+  mX = mouseX;
+  mY = mouseY;
+}
+
+function touchMoved() {
+  mX = mouseX;
+  mY = mouseY;
+  return false;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
